8cf479304cbe6a2b157daa8307a8c6dc2c8ac0b0
Paul Wagner <paulw@foodchain.com>
2017-04-05 14:46:01 -0700

    add option to disable from config file for faster dev

--- autogit.php
+++ autogit.php
@@ -10,28 +10,33 @@
  * @license   MIT
  */
 
-// Load Auto Git class and dependencies
-require_once(__DIR__.DS.'vendor'.DS.'autoload.php');
-require_once(__DIR__.DS.'lib'.DS.'autogit.php');
+if (c::get('autogit.active', true)) {
+    // Load Auto Git class and dependencies
+    require_once(__DIR__.DS.'vendor'.DS.'autoload.php');
+    require_once(__DIR__.DS.'lib'.DS.'autogit.php');
 
-// Helper function that returns an Autogit\Autogit instance
-function autogit() {
-    return Autogit\Autogit::instance();
-}
-
-// Only load hooks, routes and widgets when
-// the content directory is a Git repo
-if (autogit()->isRepo()) {
-    // Load hooks
-    require_once(__DIR__.DS.'lib'.DS.'hooks.php');
-
-    // Load routes
-    if (c::get('autogit.webhook.secret', false) and autogit()->hasRemote()) {
-        require_once(__DIR__.DS.'lib'.DS.'routes.php');
+    // Helper function that returns an Autogit\Autogit instance
+    function autogit() {
+        return Autogit\Autogit::instance();
     }
 
-    // Load widgets
-    if (c::get('autogit.widget', true)) {
-        kirby()->set('widget', 'autogit', __DIR__.DS.'widgets'.DS.'autogit');
+
+
+
+    // Only load hooks, routes and widgets when
+    // the content directory is a Git repo
+    if (autogit()->isRepo()) {
+        // Load hooks
+        require_once(__DIR__.DS.'lib'.DS.'hooks.php');
+
+        // Load routes
+        if (c::get('autogit.webhook.secret', false) and autogit()->hasRemote()) {
+            require_once(__DIR__.DS.'lib'.DS.'routes.php');
+        }
+
+        // Load widgets
+        if (c::get('autogit.widget', true)) {
+            kirby()->set('widget', 'autogit', __DIR__.DS.'widgets'.DS.'autogit');
+        }
     }
 }
